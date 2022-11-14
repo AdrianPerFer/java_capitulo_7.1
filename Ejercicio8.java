8b42a3213e19ed698b33d7c87492ecfc39f4ffb3
Adrian <chopon007@gmail.com>
2022-11-14 21:07:14 +0100

    Ejercicio8 changed

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,8 +1,12 @@
 /*
+ * 
  * Realiza un programa que pida la temperatura media que ha hecho en cada mes
  * de un determinado año y que muestre a continuación un diagrama de barras
  * horizontales con esos datos. Las barras del diagrama se pueden dibujar a base
  * de asteriscos o cualquier otro carácter.
+ * 
+ * @author: Adrián Perogil Fernández
+ * 
  */
 import java.util.Scanner;
 public class Ejercicio8 {
